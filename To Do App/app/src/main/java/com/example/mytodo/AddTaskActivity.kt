d03acd85955c36c7a11d896b1642f969c13da217
Iresh Eranga <iresheranga2020al@gmail.com>
2024-05-10 11:30:12 +0530

    create complete

--- To Do App/app/src/main/java/com/example/mytodo/AddTaskActivity.kt
+++ To Do App/app/src/main/java/com/example/mytodo/AddTaskActivity.kt
@@ -2,6 +2,7 @@ package com.example.mytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mytodo.databinding.ActivityAddTaskBinding
 
 class AddTaskActivity : AppCompatActivity() {
@@ -14,6 +15,18 @@ class AddTaskActivity : AppCompatActivity() {
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = TasksDatabaseHelper(this)
+        binding.saveButton.setOnClickListener {
+            //pick an input from user
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val task = Task(0,title,content)
+
+            //Access to the database
+            db.insertTask(task)
+            finish()
+            Toast.makeText(this,"Task Saved Successfully",Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
