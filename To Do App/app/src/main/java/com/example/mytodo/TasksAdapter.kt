f74c06e912e66156e6ea9b7ccb287d5cfe30f481
Iresh Eranga <iresheranga2020al@gmail.com>
2024-05-10 14:57:11 +0530

    update button fun

--- To Do App/app/src/main/java/com/example/mytodo/TasksAdapter.kt
+++ To Do App/app/src/main/java/com/example/mytodo/TasksAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.mytodo
 
 import android.content.Context
+import android.content.Intent
 import android.icu.text.Transliterator.Position
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +16,8 @@ class TasksAdapter(private var tasks: List<Task>, context: Context):RecyclerView
     class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+        val updateButton: ImageView = itemView.findViewById(R.id.updateSaveButton)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -27,6 +31,13 @@ class TasksAdapter(private var tasks: List<Task>, context: Context):RecyclerView
         val task = tasks[position]
         holder.titleTextView.text = task.title
         holder.contentTextView.text = task.content
+
+        holder.updateButton.setOnClickListener{
+            val intent = Intent(holder.itemView.context, UpdateActivity::class.java).apply {
+                putExtra("task_id",task.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     fun refreshData(newTasks: List<Task>){
