bb57dd9eb581d63f1300c76ce6340ec5b2d86241
Iresh Eranga <iresheranga2020al@gmail.com>
2024-05-10 10:17:44 +0530

    task adding

--- To Do App/app/src/main/java/com/example/mytodo/AddTaskActivity.kt
+++ To Do App/app/src/main/java/com/example/mytodo/AddTaskActivity.kt
@@ -2,10 +2,18 @@ package com.example.mytodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytodo.databinding.ActivityAddTaskBinding
 
 class AddTaskActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddTaskBinding
+    private lateinit var db: TasksDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_task)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
